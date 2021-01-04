697f89d5b1312344c773d1995594fe92c8a4a4b0
gaoshuang <12345678>
2021-01-04 11:45:01 +0800

    新增打包配置文件

---
+++ cicd/build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+APP_NAME=custom-newhope-data
+APP_PKG_DIR=custom-newhope-data-webapi/
+APP_PKG=custom-newhope-data-webapi
+DEST_DIR=build_dest_dir
+#参数1
+IMAGE=192.168.18.133:8080/platform2/${APP_NAME}
+#参数2
+TAG=$(date +%Y%m%d%H%M%S)
+#TAG=lastet
+
+#参数赋值
+if [ x"$1" != x ]
+then
+    IMAGE=$1
+fi
+
+if [ x"$2" != x ]
+then
+    TAG=$2
+fi
+
+#---------- 创建docker构建目录 ----------
+rm -rf  ${DEST_DIR}
+mkdir ${DEST_DIR}
+
+#---------- 复制必要的文件到Docker构建目录 ----------
+cp -r cicd/*  ${DEST_DIR}
+cp ${APP_PKG_DIR}target/${APP_PKG}.tar.gz ${DEST_DIR}/app.tar.gz
+
+#---------- 构建Docker镜像,$0为外部传入目标镜像库项目和标签路径 ----------
+rm -f docker_build_result.log
+cd ${DEST_DIR}
+docker build -t "${IMAGE}":"${TAG}" .| tee ../docker_build_result.log
+
+#建日志打印和构建结果返回
+#RESULT=$(cat ../Docker_build_result.log | tail -n 1)
+
+#if [["$RESULT" != *Successfully*]];then
+#  exit -1
+#fi
+#docker push "${IMAGE}":"${TAG}"
